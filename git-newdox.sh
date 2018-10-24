16c6ccea597937c2a715bc64133e4b0f17081d69
Jordan Moxon <jem497@cornell.edu>
2018-10-24 10:10:54 -0700

    Added minor documentation for assumptions for scripts

--- git-newdox.sh
+++ git-newdox.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# assumes that the print_newdox.sh and git-branchpoint.sh scripts are accessible
+# in the path as print_newdox and git-branchpoint, respectively
 print_newdox $(git-branchpoint)
--- git-noexcept.sh
+++ git-noexcept.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# assumes that detect_missing_noexcept.sh and git-branchpoint.sh are available
+# in the PATH as detect_missing_noexcept and git-branchpoint, respectively
 detect_missing_noexcept $(git-branchpoint)
